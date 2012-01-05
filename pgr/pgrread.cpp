fbc112353791752674bb4128d3eb7a2afba02d6b
ludflu <ludflu@gmail.com>
2012-01-04 19:41:01 -0500

    changes for key frame grabbing

--- pgr/pgrread.cpp
+++ pgr/pgrread.cpp
@@ -15,12 +15,41 @@ deque<double> moving;
 
 IplImage* lastimg;
 int c = 0;
+int frame = 1;
 const int WINDOW_SIZE= 100;
 const int START_FRAMES= 200;
 vector<double> weights;
 
 double thresh =0.0;
+double avg;
 
+const int ST_INIT = 0;
+const int ST_WAIT_MVMT = 1;
+const int ST_WAIT_STILL = 2;
+
+int state = ST_INIT;
+
+void state_machine() {
+
+        if (avg > thresh) {
+                cout << "avg: " << avg << endl;
+                cout << "c: " << c << endl;
+                cout << "movement!" << endl;
+                if (state == ST_WAIT_MVMT) {
+                        state = ST_WAIT_STILL;
+                }
+        } else {
+                if (state == ST_WAIT_STILL) {
+                        //we were waiting for stillness, and we found it, so we can advance the frame counter
+                        // and take a picture now.
+                        state = ST_WAIT_MVMT;
+                        frame++;
+                        cout << "stillness!" << endl;
+                        cout << "frame:" <<frame << endl;
+                }
+        }
+
+}
 
 void stats() {
         double sum = 0.0;
@@ -29,24 +58,20 @@ void stats() {
                 sum += (*it) * weights.at(i++);
         }
 
-        double avg = sum / (double) moving.size();
+        avg = sum / (double) moving.size();
         c++;
         if ((thresh == 0.0) && (c >START_FRAMES)) {
                 thresh = avg * 1.5;
+                state = ST_WAIT_MVMT;
         }
 
-        cout << "avg: " << avg << endl;
-        cout << "c: " << c << endl;
-        if (avg > thresh) {
-        cout << "movement!" << endl;
-        }
-
+        state_machine();
 }
 
 
 void getImage(FlyCapture2::Image *img) 
 {
-        std::cout << "entered image callback" << std::endl;
+        //std::cout << "entered image callback" << std::endl;
         FlyCapture2::Error error;
         //error = img->Save("test.pgm" );
 
@@ -58,9 +83,7 @@ void getImage(FlyCapture2::Image *img)
 
         unsigned int data_size = img->GetDataSize();
 
-        cvimg = cvCreateImage( cvSize( col, row),  
-             IPL_DEPTH_8U,  
-             1 );
+        cvimg = cvCreateImage( cvSize( col, row),  IPL_DEPTH_8U,  1 );
 
         // Copy FlyCapture2 image into OpenCV struct  
         memcpy( cvimg->imageData,  
