3931b5c000fdc4c38198b91f038371e0b9f50bc2
Jim Snavely <ludflu@gmail.com>
2012-02-26 13:35:37 -0500

    use openCV background subtraction

--- pgr/pgrread.cpp
+++ pgr/pgrread.cpp
@@ -3,87 +3,31 @@
 #include <iostream>
 
 #include "precomp.hpp"
-#include <deque>
 #include <numeric>
+
+#include <opencv2/video/background_segm.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace pgr_camera;
 using namespace cv;
 using namespace std;
 
-deque<double> moving;
 
 IplImage* lastimg;
-int c = 0;
-int frame = 1;
-const int WINDOW_SIZE= 100;
-const int START_FRAMES= 200;
-vector<double> weights;
 
-double thresh =0.0;
-double avg;
+CvBGStatModel* bg_model = NULL;
 
-const int ST_INIT = 0;
-const int ST_WAIT_MVMT = 1;
-const int ST_WAIT_STILL = 2;
+bool update_bg_model = true;
+int frame =0;
 
-const double THRESH= 1.75;
-
-int state = ST_INIT;
-
-void saveit(int frame) {
+void saveit(int frame,IplImage * img) {
   cout << "saving image" << std::endl;
   ostringstream out;
   out << "test" << frame << ".jpg";
-  cvSaveImage(out.str().c_str() ,lastimg);
+  cvSaveImage(out.str().c_str() ,img);
 }
 
-
-void state_machine() {
-
-        if (avg > thresh) {
-                cout << "avg: " << avg << endl;
-                cout << "c: " << c << endl;
-                cout << "movement!" << endl;
-                if (state == ST_WAIT_MVMT) {
-                        state = ST_WAIT_STILL;
-                }
-        } else {
-                if (state == ST_WAIT_STILL) {
-                        //we were waiting for stillness, and we found it, so we can advance the frame counter
-                        // and take a picture now.
-                        state = ST_WAIT_MVMT;
-                        frame++;
-			saveit(frame);
-                        cout << "stillness!" << endl;
-                        cout << "frame:" <<frame << endl;
-			cout<<(char)7<<endl;
-                }
-        }
-
-}
-
-void stats() {
-        double sum = 0.0;
-        int i = 0       ;
-        for ( deque<double>::iterator it = moving.begin(); it != moving.end(); it++ ) {
-                sum += (*it) * weights.at(i++);
-        }
-
-        avg = sum / (double) moving.size();
-        c++;
-        if ((thresh == 0.0) && (c >START_FRAMES)) {
-                thresh = avg * THRESH;
-                state = ST_WAIT_MVMT;
-		cout<<(char)7<<endl;
-	        saveit(frame);
-        }
-
-        state_machine();
-}
-
-
-
-void getImage(FlyCapture2::Image *img) 
+IplImage *copyImage(FlyCapture2::Image *img) 
 {
 
         IplImage* cvimg;
@@ -99,42 +43,46 @@ void getImage(FlyCapture2::Image *img)
         memcpy( cvimg->imageData,  
         img->GetData(),  
         data_size ); 
-        
-        double n = cvNorm(cvimg,lastimg, CV_RELATIVE_L2 );
-
-	//release old cvimg before you do this - right now you're leaking memory!!!! JCS TODO
-	cvReleaseImage(&lastimg);
-
-
-        lastimg = cvimg;
-        moving.push_front(n);
-        if (moving.size() > WINDOW_SIZE ) {
-                moving.pop_back();
-        }       
-        stats();
+        return cvimg;
 }
 
 
-vector<double> decay( int count) {
+void getImage(FlyCapture2::Image *img) { 
+        frame++;
+    
+        IplImage* cvimg  = copyImage(img);
 
-        vector<double> weights;
+            //create BG model
+         if (bg_model == NULL ) {
+               cout <<"creating model" << endl;
 
-        for (double w=WINDOW_SIZE; w>=1; w--) {
-                double d = w / WINDOW_SIZE ;
-                cout << "d:" << d << endl;
-                weights.push_back(d);
-        }
-        return weights;
+               // Select parameters for Gaussian model.
+                CvGaussBGStatModelParams* params = new CvGaussBGStatModelParams;                     
+                params->win_size=2;  
+                params->n_gauss=5;
+                params->bg_threshold=0.7;
+                params->std_threshold=3.5;
+                params->minArea=15;
+                params->weight_init=0.05;
+                params->variance_init=30; 
+            bg_model = cvCreateGaussianBGModel( cvimg, params );
+        int r = cvUpdateBGStatModel( cvimg, bg_model );
+            return;
+         }
 
+        int r = cvUpdateBGStatModel( cvimg, bg_model );
+         cvShowImage("FG", bg_model->foreground);
+         cvShowImage("BG", bg_model->background);
+        cvWaitKey(100); //we need to do this, otherwise the window doesn't get updated
 }
 
 int main( int argc, const char* argv[] ) {
 
-        weights = decay(WINDOW_SIZE);
-
-	lastimg = cvCreateImage( cvSize( 640, 480),  IPL_DEPTH_8U,  1 );
+        lastimg = cvCreateImage( cvSize( 640, 480),  IPL_DEPTH_8U,  1 );
 
 
+        cvNamedWindow("FG");
+        cvNamedWindow("BG");
 
         Camera *camera = new Camera();
         camera->setFrameCallback(getImage);
@@ -149,5 +97,7 @@ int main( int argc, const char* argv[] ) {
 
         camera->stop();
         std::cout << "stop capture" << std::endl;
+         cvReleaseBGStatModel( &bg_model );
+
 }
 
